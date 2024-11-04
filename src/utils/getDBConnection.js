c8757e35e99a012e48f9f8da7d36d57c410ad271
lucasrochabz <lucasbezerrar@gmail.com>
2024-11-04 14:36:53 -0300

    pequenos ajustes

--- src/utils/getDBConnection.js
+++ src/utils/getDBConnection.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql2/promise');
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 
 const getDBConnection = async () => {
   const connection = await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_NAME,
     port: process.env.DB_PORT,
   });
   return connection;
