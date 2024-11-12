0edab329a9c6127900b0ee0612fc75d11d5fb9f9
lucasrochabz <lucasbezerrar@gmail.com>
2024-11-12 12:39:34 -0300

    pequenos ajustes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
 
 const productsRouter = require('./routes/products');
 const usersRouter = require('./routes/users');
+
+app.use(express.json());
 
 app.use('/products', productsRouter);
 app.use('/users', usersRouter);
