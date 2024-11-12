ec6f695b71a774d3b7520a0ae94a1e1258e46290
lucasrochabz <lucasbezerrar@gmail.com>
2024-11-12 11:56:13 -0300

    add bloco try e finally

--- src/models/users.js
+++ src/models/users.js
@@ -2,51 +2,71 @@ const { getDBConnection } = require('../utils/getDBConnection');
 
 const getAllUsers = async () => {
   const connection = await getDBConnection();
-  const [results] = await connection.query(
-    `
-    SELECT * FROM users`,
-  );
-  return results;
+  try {
+    const [results] = await connection.query(
+      `
+      SELECT * FROM users`,
+    );
+    return results;
+  } finally {
+    await connection.end();
+  }
 };
 
 const getOneUser = async (userId) => {
   const connection = await getDBConnection();
-  const [results] = await connection.query(
-    `
-    SELECT * FROM users WHERE id=?`,
-    [userId],
-  );
-  return results;
+  try {
+    const [results] = await connection.query(
+      `
+      SELECT * FROM users WHERE id=?`,
+      [userId],
+    );
+    return results;
+  } finally {
+    await connection.end();
+  }
 };
 
 const postOneUser = async (name, email) => {
   const connection = await getDBConnection();
-  const [results] = await connection.query(
-    `
-    INSERT INTO users (name, email) VALUES (?,?)`,
-    [name, email],
-  );
-  return results;
+  try {
+    const [results] = await connection.query(
+      `
+      INSERT INTO users (name, email) VALUES (?,?)`,
+      [name, email],
+    );
+    return results;
+  } finally {
+    await connection.end();
+  }
 };
 
 const putOneUser = async (name, userId) => {
   const connection = await getDBConnection();
-  const [results] = await connection.query(
-    `
-    UPDATE users SET name=? WHERE id=?`,
-    [name, userId],
-  );
-  return results;
+  try {
+    const [results] = await connection.query(
+      `
+      UPDATE users SET name=? WHERE id=?`,
+      [name, userId],
+    );
+    return results;
+  } finally {
+    await connection.end();
+  }
 };
 
 const deleteOneUser = async (userId) => {
   const connection = await getDBConnection();
-  const [results] = await connection.query(
-    `
-    DELETE FROM users WHERE id=?`,
-    [userId],
-  );
-  return results;
+  try {
+    const [results] = await connection.query(
+      `
+      DELETE FROM users WHERE id=?`,
+      [userId],
+    );
+    return results;
+  } finally {
+    await connection.end();
+  }
 };
 
 module.exports = {
