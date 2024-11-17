b3c0eddb4828f8dbe8c57e54cc12cf01ff226684
lucasrochabz <lucasbezerrar@gmail.com>
2024-11-17 13:16:15 -0300

    add bloco try catch a conexao ao banco de dados

--- src/utils/getDBConnection.js
+++ src/utils/getDBConnection.js
@@ -2,14 +2,19 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const getDBConnection = async () => {
-  const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-  });
-  return connection;
+  try {
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+    });
+
+    return connection;
+  } catch (error) {
+    console.error('Erro ao conectar ao banco de dados:', error);
+  }
 };
 
 module.exports = { getDBConnection };
