251fd7391cf6e39d03a6bdf2c9ea7c4100bdc063
lucasrochabz <lucasbezerrar@gmail.com>
2024-11-26 11:16:08 -0300

    atualizando script para criar tabelas

--- execute-scripts.js
+++ execute-scripts.js
@@ -1,45 +1,45 @@
-const fs = require('fs');
+const fs = require('fs/promises');
+const mysql = require('mysql2/promise');
 const path = require('path');
-const mysql = require('mysql2');
 
 // Configuração do banco de dados
-const connection = mysql.createConnection({
+const connectionConfig = {
   host: 'localhost',
   user: 'seu_usuario',
   password: 'sua_senha',
   database: 'nome_do_banco',
-});
-// Certifique-se de criar o banco antes de rodar este script
+};
 
 // Caminho para a pasta com os scripts SQL
 const scriptsPath = path.join(__dirname, 'sql-scripts');
 
 // Função para executar os scripts SQL
 const executeScripts = async () => {
-  fs.readdir(scriptsPath, (err, files) => {
-    if (err) {
-      console.error('Erro ao ler a pasta de scripts:', err);
-      return;
-    }
+  try {
+    // Conect ao banco de dados
+    const connection = await mysql.createConnection(connectionConfig);
 
+    // Lê os arquivos da pasta
+
+    const files = await fs.readdir(scriptsPath);
     const sqlFiles = files.filter((file) => file.endsWith('.sql'));
 
-    sqlFiles.forEach((file) => {
+    for (const file of sqlFiles) {
       const filePath = path.join(scriptsPath, file);
-      const sql = fs.readFileSync(filePath, 'utf8');
+      const sql = await fs.readFile(filePath, 'utf8');
 
-      connection.query(sql, (queryErr) => {
-        if (queryErr) {
-          console.error(`Erro ao executar o script ${file}:`, queryErr.message);
-        } else {
-          console.log(`Script ${file} executado com sucesso.`);
-        }
-      });
-    });
-
-    // Fechar a conexão após terminar
-    connection.end();
-  });
+      try {
+        await connection.query(sql);
+        console.log(`Script ${file} executado com sucesso.`);
+      } catch (queryErr) {
+        console.error(`Erro ao executar o script ${file}:`, queryErr.message);
+      }
+    }
+    // Fecha a conexão
+    await connection.end();
+  } catch (error) {
+    console.error('Erro ao executar os scripts:', error.message);
+  }
 };
 
 // Executar os scripts
