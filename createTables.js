d22f745c52692f4ab54b22d1716ac5caabf41dc4
lucasrochabz <lucasbezerrar@gmail.com>
2024-11-27 13:22:53 -0300

    alterando pastas

---
+++ createTables.js
@@ -0,0 +1,48 @@
+const fs = require('fs/promises');
+const mysql = require('mysql2/promise');
+const path = require('path');
+require('dotenv').config();
+
+// Configuração do banco de dados
+const connectionConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+};
+
+// Caminho para a pasta com os scripts SQL
+const scriptsPath = path.join(__dirname, 'sql_scripts/create_tables');
+
+// Função para executar os scripts SQL
+const executeTables = async () => {
+  try {
+    // Conecta ao banco de dados
+    const connection = await mysql.createConnection(connectionConfig);
+
+    // Lê os arquivos da pasta
+    const files = await fs.readdir(scriptsPath);
+    const sqlFiles = files.filter((file) => file.endsWith('.sql'));
+
+    for (const file of sqlFiles) {
+      const filePath = path.join(scriptsPath, file);
+      const sql = await fs.readFile(filePath, 'utf8');
+
+      try {
+        await connection.query(sql);
+        console.log(`Script ${file} executado com sucesso.`);
+      } catch (queryErr) {
+        console.error(`Erro ao executar o script ${file}:`, queryErr.message);
+      }
+    }
+
+    // Fecha a conexão
+    await connection.end();
+  } catch (error) {
+    console.error('Erro ao executar os scripts:', error.message);
+  }
+};
+
+// Executar os scripts
+executeTables();
--- insertDatas.js
+++ insertDatas.js
@@ -12,37 +12,40 @@ const connectionConfig = {
   port: process.env.DB_PORT,
 };
 
-// Caminho para a pasta com os scripts SQL
-const scriptsPath = path.join(__dirname, 'sql-scripts/create-tables');
+// Caminho para a pasta com os scripts de inserção de dados
+const insertsPath = path.join(__dirname, 'sql_scripts/insert_datas');
 
-// Função para executar os scripts SQL
-const executeScripts = async () => {
+// Função para executar os inserts
+const executeInserts = async () => {
   try {
     // Conecta ao banco de dados
     const connection = await mysql.createConnection(connectionConfig);
 
     // Lê os arquivos da pasta
-    const files = await fs.readdir(scriptsPath);
+    const files = await fs.readdir(insertsPath);
     const sqlFiles = files.filter((file) => file.endsWith('.sql'));
 
     for (const file of sqlFiles) {
-      const filePath = path.join(scriptsPath, file);
+      const filePath = path.join(insertsPath, file);
       const sql = await fs.readFile(filePath, 'utf8');
 
       try {
         await connection.query(sql);
-        console.log(`Script ${file} executado com sucesso.`);
+        console.log(`Dados do script ${file} inseridos com sucesso.`);
       } catch (queryErr) {
-        console.error(`Erro ao executar o script ${file}:`, queryErr.message);
+        console.error(
+          `Erro ao inserir dados do script ${file}:`,
+          queryErr.message,
+        );
       }
     }
 
     // Fecha a conexão
     await connection.end();
   } catch (error) {
-    console.error('Erro ao executar os scripts:', error.message);
+    console.error('Erro ao executar os inserts:', error.message);
   }
 };
 
-// Executar os scripts
-executeScripts();
+// Executar os inserts
+executeInserts();
