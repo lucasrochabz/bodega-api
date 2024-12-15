c1047f3b27a715e4b717bc0e8b18e49850d6fe0c
lucasrochabz <lucasbezerrar@gmail.com>
2024-12-15 17:17:33 -0300

    pequeno ajuste

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const app = require('./src/app');
 const API_PORT = process.env.API_PORT || 3000;
 
 app.listen(API_PORT, () => {
-  console.log(`Servidor rodando na porta ${API_PORT}`);
+  console.log(`Executando servidor na porta ${API_PORT}`);
 });
