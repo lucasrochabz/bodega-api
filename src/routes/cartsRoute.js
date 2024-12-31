72fbdf2f54a7c50fb51a6c63a69563cee5edb009
lucasrochabz <lucasbezerrar@gmail.com>
2024-12-31 10:21:36 -0300

    pequenos ajustes

--- src/routes/cartsRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { cartsController } = require('../controllers/cartsController');
-
-router.put('/addToCart', cartsController.getCarts);
-
-module.exports = router;
--- src/teste-model/teste-products.js
+++ src/teste-model/teste-products.js
@@ -1,13 +1,47 @@
-class Products {
-  constructor(id, name, price, description, stock, status, image_path) {
-    this.id = id;
-    this.name = name;
-    this.price = price;
-    this.description = description;
-    this.stock = stock;
-    this.status = status;
-    this.image_path = image_path;
-  }
-}
+// class Products {
+//   constructor(id, name, price, description, stock, status, image_path) {
+//     this.id = id;
+//     this.name = name;
+//     this.price = price;
+//     this.description = description;
+//     this.stock = stock;
+//     this.status = status;
+//     this.image_path = image_path;
+//   }
+// }
 
-module.exports = Products;
+// module.exports = Products;
+
+const createProduct = (
+  id,
+  name,
+  price,
+  description,
+  stock,
+  status,
+  imagePath,
+) => ({
+  id,
+  name,
+  price,
+  description,
+  stock,
+  status,
+  imagePath,
+});
+
+module.exports = createProduct;
+
+// uso
+const createProduct = require('./models/productModel');
+
+const newProduct = createProduct(
+  null,
+  'Produto X',
+  100.0,
+  'Descrição do produto',
+  10,
+  'ativo',
+  'path/to/image.jpg',
+);
+console.log(newProduct);
