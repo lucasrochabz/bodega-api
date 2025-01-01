e45c6aa9bcc086966148bf918705f7b5f58b8bdb
lucasrochabz <lucasbezerrar@gmail.com>
2024-12-31 21:00:02 -0300

    pequeno ajuste

--- sql_scripts/insert_datas/3_products_data.sql
+++ sql_scripts/insert_datas/3_products_data.sql
@@ -3,7 +3,7 @@ INSERT INTO products
   VALUES
   ('Notebook', 1500.00, 'O melhor Notebook', 10, 'ativo', 'notebook-1.jpg'),
   ('Smartphone', 2000.50, 'O melhor Smartphone', 10, 'ativo', 'smartphone-1.jpg'),
-  ('Câmera', 2500.00, 'A melhor Camera', 10, 'ativo', 'camera-1.jpg'),
+  ('Câmera', 2500.00, 'A melhor Câmera', 10, 'ativo', 'camera-1.jpg'),
   ('Smartwatch', 3000.50, 'O melhor Smartwatch', 10, 'ativo', 'smartwatch-1.jpg'),
   ('Tablet', 3500.00, 'O melhor Tablet', 10, 'ativo', 'tablet-1.jpg'),
   ('Smartspeaker', 4000.50, 'O melhor Smartspeaker', 10, 'ativo', 'smartspeaker-1.jpg'),
