bb8cf81cd0ac22ecfd076f334d15193e9328ae36
lucasrochabz <lucasbezerrar@gmail.com>
2025-01-03 23:21:57 -0300

    corrigindo textos dos produtos

--- sql_scripts/insert_datas/3_products_data.sql
+++ sql_scripts/insert_datas/3_products_data.sql
@@ -1,10 +1,10 @@
 INSERT INTO products
   (name, price, description, stock, status, image_path)
   VALUES
-  ('Notebook', 1500.00, 'O melhor Notebook', 10, 'ativo', 'notebook-1.jpg'),
-  ('Smartphone', 2000.50, 'O melhor Smartphone', 10, 'ativo', 'smartphone-1.jpg'),
-  ('Câmera', 2500.00, 'A melhor Câmera', 10, 'ativo', 'camera-1.jpg'),
-  ('Smartwatch', 3000.50, 'O melhor Smartwatch', 10, 'ativo', 'smartwatch-1.jpg'),
-  ('Tablet', 3500.00, 'O melhor Tablet', 10, 'ativo', 'tablet-1.jpg'),
-  ('Smartspeaker', 4000.50, 'O melhor Smartspeaker', 10, 'ativo', 'smartspeaker-1.jpg'),
-  ('Teste', 4500.00, 'O melhor teste', 10, 'ativo', 'default_image.png');
+  ('Notebook', 1500.00, 'O Notebook é ideal para quem busca desempenho e portabilidade em um único aparelho.', 10, 'ativo', 'notebook-1.jpg'),
+  ('Smartphone', 2000.50, 'O Smartphone oferece tecnologia avançada e recursos inovadores para facilitar o seu dia a dia.', 10, 'ativo', 'smartphone-1.jpg'),
+  ('Câmera', 2500.00, 'A Câmera proporciona imagens de alta qualidade, capturando momentos únicos com precisão incrível.', 10, 'ativo', 'camera-1.jpg'),
+  ('Smartwatch', 3000.50, 'O Smartwatch combina estilo e funcionalidade, monitorando sua saúde e conectando você ao mundo.', 10, 'ativo', 'smartwatch-1.jpg'),
+  ('Tablet', 3500.00, 'O Tablet é perfeito para entretenimento e trabalho, oferecendo versatilidade em um design compacto.', 10, 'ativo', 'tablet-1.jpg'),
+  ('Smartspeaker', 4000.50, 'O Smartspeaker é uma solução inteligente para controle de dispositivos e reprodução de mídia.', 10, 'ativo', 'smartspeaker-1.jpg'),
+  ('Teste', 4500.00, 'Este produto serve como teste para o sistema.', 10, 'ativo', 'default_image.png');
