be309706cb3c2f9b38ec9cb83e81f8b293fbf6fa
lucasrochabz <lucasbezerrar@gmail.com>
2025-01-31 01:05:37 -0300

    alterando status de insert orders

--- sql_scripts/insert_datas/4_orders_data.sql
+++ sql_scripts/insert_datas/4_orders_data.sql
@@ -1,6 +1,6 @@
 INSERT INTO orders
   (user_id, address_id, created_at, status)
   VALUES
-  (1, 1, '2024-11-07 14:30:00', 'rascunho'),
-  (2, 2, '2024-11-07 14:30:00', 'rascunho'),
-  (3, 3, '2024-11-07 14:30:00', 'rascunho');
+  (1, 1, '2024-11-07 14:30:00', 'aguardando pagamento'),
+  (2, 2, '2024-11-07 14:30:00', 'aguardando pagamento'),
+  (3, 3, '2024-11-07 14:30:00', 'aguardando pagamento');
