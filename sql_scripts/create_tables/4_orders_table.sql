f70f5461f0e4d1b2c6147387a5ffa559bfc5d29c
lucasrochabz <lucasbezerrar@gmail.com>
2025-02-20 11:48:44 -0300

    add on delete cascade a tabela orders

--- sql_scripts/create_tables/4_orders_table.sql
+++ sql_scripts/create_tables/4_orders_table.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS orders (
 	address_id INT NOT NULL,
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	status ENUM('rascunho', 'aguardando pagamento', 'pagamento efetuado', 'concluido') NOT NULL,
-	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
 	FOREIGN KEY (address_id) REFERENCES addresses(id)
 );
