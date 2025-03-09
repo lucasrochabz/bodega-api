178dd840722884766e0657e4942f574df63f226a
lucasrochabz <lucasbezerrar@gmail.com>
2025-03-09 18:20:23 -0300

    correção para deploy

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const app = require('./src/app');
 
-const API_PORT = process.env.API_PORT || 3000;
+// const API_PORT = process.env.API_PORT || 3000;
 
-app.listen(API_PORT, () => {
-  console.log(`Executando servidor na porta ${API_PORT}`);
-});
+// app.listen(API_PORT, () => {
+//   console.log(`Executando servidor na porta ${API_PORT}`);
+// });
