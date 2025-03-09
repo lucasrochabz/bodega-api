f86c18b95949478f2a3179167c966216461679ac
lucasrochabz <lucasbezerrar@gmail.com>
2025-03-09 19:09:39 -0300

    pequeno ajuste

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const serverless = require('serverless-http');
 const app = require('./src/app');
 
-module.exports.handler = serverless(app);
+module.exports = serverless(app);
 
 // const API_PORT = process.env.API_PORT || 3000;
 
