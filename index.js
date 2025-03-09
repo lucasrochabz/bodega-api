ff10aef1f3c6277f4f16eba2523d46546a11be58
lucasrochabz <lucasbezerrar@gmail.com>
2025-03-09 18:31:16 -0300

    add arquivo vercel.json

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
+const serverless = require('serverless-http');
 const app = require('./src/app');
+
+module.exports.handler = serverless(app);
 
 // const API_PORT = process.env.API_PORT || 3000;
 
