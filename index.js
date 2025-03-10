97446ce3587d6a3b2c500e8c85c2bb38a0ce41d1
lucasrochabz <lucasbezerrar@gmail.com>
2025-03-09 23:57:28 -0300

    ajuste final

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// require('dotenv').config();
 const app = require('./src/app');
 
 module.exports = app;
-
-// const API_PORT = process.env.API_PORT || 3000;
-
-// app.listen(API_PORT, () => {
-//   console.log(`Executando servidor na porta ${API_PORT}`);
-// });
