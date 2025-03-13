a091f6305c41ba553db812e2e5e4dda2fbd19943
lucasrochabz <lucasbezerrar@gmail.com>
2025-03-13 13:10:15 -0300

    add created_at e updated_at nas tabelas

--- sql_scripts/create_tables/4_orders_table.sql
+++ sql_scripts/create_tables/4_orders_table.sql
@@ -2,8 +2,9 @@ CREATE TABLE IF NOT EXISTS orders (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	user_id INT NOT NULL, 
 	address_id INT NOT NULL,
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	status ENUM('rascunho', 'aguardando pagamento', 'pagamento efetuado', 'concluido') NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
 	FOREIGN KEY (address_id) REFERENCES addresses(id)
 );
