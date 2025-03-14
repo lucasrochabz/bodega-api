347c8f84003ec051b6e22513222ebe69d802d06e
lucasrochabz <lucasbezerrar@gmail.com>
2025-03-14 20:24:30 -0300

    configurando ambiente de produção

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,11 +4,11 @@ const mysql = require('mysql2/promise');
 const getDBConnection = async () => {
   try {
     const connection = await mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
+      host: process.env.MYSQLHOST,
+      user: process.env.MYSQLUSER,
+      password: process.env.MYSQLPASSWORD,
       database: process.env.DB_NAME,
-      port: process.env.DB_PORT,
+      port: process.env.MYSQLPORT,
     });
 
     return connection;
