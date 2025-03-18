63c1a5295447055df24603ca23586cde95d98f7b
lucasrochabz <lucasbezerrar@gmail.com>
2025-03-18 12:39:31 -0300

    pequeno ajuste

--- src/repositories/usersRepository.js
+++ src/repositories/usersRepository.js
@@ -60,7 +60,7 @@ const usersRepository = {
         `
         INSERT INTO users (first_name, last_name, email, password)
         VALUES (?, ?, ?, ?)`,
-        [user.name, user.last_name, user.email, user.password],
+        [user.first_name, user.last_name, user.email, user.password],
       );
 
       return results;
