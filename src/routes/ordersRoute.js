f4903d30e79fa1ef3f7282dbe4da4ad76326c8c7
lucasrochabz <lucasbezerrar@gmail.com>
2025-04-08 16:24:28 -0300

    corrigindo rota de pedidos

--- src/routes/ordersRoute.js
+++ src/routes/ordersRoute.js
@@ -5,7 +5,7 @@ const { ordersController } = require('../controllers/ordersController');
 
 router.get('/', ordersController.getAllOrders);
 router.get('/user', authenticate, ordersController.getUserOrders);
-router.get('/details/:orderId', ordersController.getOrderDetails);
+router.get('/:orderId', ordersController.getOrderDetails);
 router.post('/', authenticate, ordersController.createOrder);
 router.put('/:orderId', ordersController.updateOrder);
 router.delete('/:orderId', ordersController.deleteOrder);
