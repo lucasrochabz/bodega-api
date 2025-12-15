76f07e63e0b30cf019e0335acb7b95864b3254fc
lucasrochabz <lucasbezerrar@gmail.com>
2025-12-15 00:38:07 -0300

    ajuste ao CORS

--- src/app.js
+++ src/app.js
@@ -10,11 +10,25 @@ const allowedOrigins = [
   process.env.ORIGIN_PROD,
 ];
 
+const allowedHeaders = ['Content-Type', 'Authorization'];
+
 app.use(express.json());
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: (origin, callback) => {
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error('Not allowed by CORS'));
+    },
+
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: allowedHeaders,
   }),
 );
 
