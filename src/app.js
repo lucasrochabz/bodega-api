ccf447ff6a8de74d0f323512a772b9dba7485faa
lucasrochabz <lucasbezerrar@gmail.com>
2025-12-14 21:46:44 -0300

    pequeno ajuste

--- src/app.js
+++ src/app.js
@@ -4,10 +4,16 @@ import apiRouter from './routes/index.js';
 
 export const app = express();
 
+const allowedOrigins = [
+  process.env.ORIGIN_DEV,
+  process.env.ORIGIN_BUILD,
+  process.env.ORIGIN_PROD,
+];
+
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.ALLOWED_ORIGINS,
+    origin: allowedOrigins,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   }),
 );
