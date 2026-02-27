f77d998505da53d46439cfe6fe2981a188482726
lucasrochabz <lucasbezerrar@gmail.com>
2026-02-27 14:29:58 -0300

    add consulta a api do viacep

---
+++ src/errors/addressErrors.js
@@ -0,0 +1,19 @@
+export const AddressErrors = {
+  INVALID_CEP: {
+    statusCode: 400,
+    code: 'INVALID_CEP',
+    message: 'CEP inválido.',
+  },
+
+  CEP_NOT_FOUND: {
+    statusCode: 404,
+    code: 'CEP_NOT_FOUND',
+    message: 'CEP não encontrado.',
+  },
+
+  VIA_CEP_UNAVAILABLE: {
+    statusCode: 400,
+    code: 'VIA_CEP_UNAVAILABLE',
+    message: 'Serviço de CEP indispónivel.',
+  },
+};
